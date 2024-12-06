0df4a7d7e2ca349c5dd9c3db795b4b44f73ceba3
LuoYuXuanRyan <1505172926@qq.com>
2024-12-07 00:00:07 +0800

    fixed render bug, renamed content directory to data

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,8 @@
 import { z, defineCollection } from 'astro:content';
-import { glob, file } from 'astro/loaders';
+import { glob } from 'astro/loaders';
 
 const posts = defineCollection({
-    loader: glob({ pattern: '**/*.md', base: './src/content/posts' }),
+    loader: glob({ pattern: '**/*.md', base: './src/data/posts' }),
     schema: z.object({
         title: z.string(),
         tags: z.array(z.string()),
