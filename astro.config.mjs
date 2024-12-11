22910e93f87b0d41892db9232c95e6d41d298460
LuoYuXuanRyan <1505172926@qq.com>
2024-12-11 11:02:08 +0800

    add mathjax plugin. adjust typography

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,21 @@
 // @ts-check
 import { defineConfig, passthroughImageService } from 'astro/config';
+import remarkMath from 'remark-math';
+import rehypeMathjax from 'rehype-mathjax';
 
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  markdown: {
-    shikiConfig: {
-      theme: 'one-dark-pro',
+    integrations: [tailwind()],
+    markdown: {
+        shikiConfig: {
+            theme: 'one-dark-pro',
+        },
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [rehypeMathjax],
     },
-  },
-  image: {
-    service: passthroughImageService()
-  },
+    image: {
+        service: passthroughImageService(),
+    },
 });
