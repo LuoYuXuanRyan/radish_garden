7e506ec87fa36a779bf1373da70d04d02ccf67b9
LuoYuXuanRyan <1505172926@qq.com>
2026-01-11 17:05:54 +0800

    feat: adjust animations of tag cards

--- src/config/animations.ts
+++ src/config/animations.ts
@@ -14,3 +14,10 @@ export const fadeInSlideViewport = {
     viewport: { once: true, margin: "-50px" },
     transition: { duration: 0.4 }
 };
+
+export const fadeInUpViewport = {
+    initial: { opacity: 0, y: 20 },
+    whileInView: { opacity: 1, y: 0 },
+    viewport: { once: true, margin: "-50px" },
+    transition: { duration: 0.3 }
+};
